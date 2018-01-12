4048404314655424245c46e1c6635acb0229ff1d
Khanh Nguyen Viet <khanhnv@nttdata.com.vn>
2018-01-12 17:12:46 +0700

    Show League and Teams

--- src/app/mock-teams.ts
+++ src/app/mock-teams.ts
@@ -4,5 +4,14 @@ export const EPL_TEAMS: Team[] = [
   { name: 'Man City', matches: 22, points: 62},
   { name: 'Man Utd', matches: 22, points: 47},
   { name: 'Chelsea', matches: 22, points: 46},
-  { name: 'Liverpool', matches: 22, points: 44}
+  { name: 'Liverpool', matches: 22, points: 44},
+  { name: 'Tottenham Hotspurs', matches: 22, points: 41}
+]
+
+export const LALIGA_TEAMS: Team[] = [
+  { name: 'Barcelona', matches: 18, points: 48 },
+  { name: 'Atletico de Madrid', matches: 18, points: 39},
+  { name: 'Valencia', matches: 18, points: 37},
+  { name: 'Real Madrid', matches: 17, points: 32},
+  { name: 'Sevilla', matches: 18, points: 29}
 ]
